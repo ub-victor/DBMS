1700c3ba1c68429c53ed2bbdb8dd2f71b267ad8b
ub-victor <victoireushindi371@gmail.com>
2026-03-17 18:55:19 +0200

    Mysql code

--- MYSQL-Workbench/Student_Management_System.sql
+++ MYSQL-Workbench/Student_Management_System.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE student_system;
 USE student_system;
 
+-- Create Database : Create the project  Storage
+-- Use : Help to active/select that Created db and allow future activities goes inside it
+
+-- Create Student table
